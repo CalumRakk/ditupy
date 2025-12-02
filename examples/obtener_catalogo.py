ff50e278815a451fb294c18f533dc0638f002d48
Leo <leocasti2@gmail.com>
2025-12-02 11:22:18 -0500

    feat: elimina ejemplos obsoletos de scripts de cliente Ditu

--- examples/obtener_catalogo.py
+++
@@ -1,9 +0,0 @@
-from ditupy.ditu import DituClient
-
-client = DituClient()
-
-for i, programa in enumerate(client.get_catalog_iterator()):
-    print(f"[{i}] {programa.title} (ID: {programa.contentId})")
-
-    if "Desafío" in programa.title:
-        print(f"¡ENCONTRADO! En colección {programa.source_collection_id}")
